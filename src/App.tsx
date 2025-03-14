620339468a4acf40e7f02f074a57262e650ad1b2
Yan Carlos <yan312005@gmail.com>
2025-03-14 16:16:56 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./global.css";
+
+import { RouterProvider } from "react-router-dom";
+
+import { router } from "./routes";
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
